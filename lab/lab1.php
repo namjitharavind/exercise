20ebcc43e03956a899aa1d8b492823d5b0f119c6
namjith aravind <namjitharavind@gmail.com>
2014-10-16 12:13:31 +0530

    existing and lab folder

---
+++ lab/lab1.php
@@ -0,0 +1,118 @@
+<?php
+class ShopProductWriter {
+
+    public $products = array();
+
+    public function addProduct(ShopProduct $shopProduct) {
+        $this->products[] = $shopProduct;
+    }
+
+    public function write($shopProduct) {
+        $str = "";
+        foreach ($this->products as $shopProduct) {
+            $str.="{$shopProduct->title}: ";
+            $str.=$shopProduct->getProducer();
+            $str.="({$shopProduct->getPrice()})\n";
+        }
+        print $str;
+    }
+
+}
+
+class ShopProduct {
+
+    private $title;
+    private $producerMainName;
+    private $producerFirstName;
+    protected $price;
+    private $discount = 0;
+
+    public function __construct($title, $firstName, $mainName, $price) {
+        $this->title = $title;
+        $this->producerFirstName = $firstName;
+        $this->producerMainName = $mainName;
+        $this->price = $price;
+    }
+
+    public function getProducerFirstName() {
+        return $this->producerFirstName;
+    }
+
+    public function getProducerMainName() {
+        return $this->producerMainName;
+    }
+
+    public function setDiscount($num) {
+        $this->discount = $num;
+    }
+
+    public function getDiscount() {
+        return $this->discount;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function getPrice() {
+        return ($this->price);
+    }
+
+    public function getProducer() {
+        return "{$this->producerFirstName}" . " " . "{$this->producerMainName}";
+    }
+
+    public function getSummaryLine() {
+        $base = "{$this->title} ( {$this->producerMainName}, ";
+        $base .= "{$this->producerFirstName} )";
+        return $base;
+    }
+
+}
+
+class CdProduct extends ShopProduct {
+
+    private $playLength = 0;
+
+    function __construct($title, $firstName, $mainName, $price, $playLength) {
+
+        parent::__construct($title, $firstName, $mainName, $price);
+        $this->playLength = $playLength;
+    }
+
+    function getPlayLength() {
+        return $this->playLength;
+    }
+
+    function getSummaryLine() {
+        $base = parent::getSummaryLine();
+        $base .= ": playing time - {$this->playLength}";
+        return $base;
+    }
+
+}
+
+class BookProduct extends ShopProduct {
+
+    private $numPages = 0;
+
+    public function __construct($title, $firstName, $mainName, $price) {
+        parent::__construct($title, $firstName, $mainName, $price);
+        $this->numPages = $numPages;
+    }
+
+    public function getNumberOfPages() {
+        return $this->numPages;
+    }
+
+    public function getSummaryLine() {
+        $base = parent::getSummaryLine();
+        $base .= ": page count - {$this->numPages}";
+        return $base;
+    }
+
+    public function getPrice() {
+        return $this->price;
+    }
+
+}
