3fa16fe38ac16416eb2fb949ef30da3db4aac4a1
namjith aravind <namjitharavind@gmail.com>
2014-12-10 18:13:48 +0530

    composite_pattern

--- lab/composite_pattern.php
+++ lab/composite_pattern.php
@@ -1,11 +1,27 @@
 <?php
 
+class UnitException extends Exception {
+    
+}
+
 abstract class Unit {
+
+    abstract function addUnit(Unit $unit);
+
+    abstract function removeUnit(Unit $unit);
 
     abstract function bombardStrength();
 }
 
 class Archer extends Unit {
+
+    function addUnit(Unit $unit) {
+        throw new UnitException(get_class($this) . " is a leaf");
+    }
+
+    function removeUnit(Unit $unit) {
+        throw new UnitException(get_class($this) . " is a leaf");
+    }
 
     function bombardStrength() {
         return 4;
@@ -15,34 +31,65 @@ class Archer extends Unit {
 
 class LaserCannonUnit extends Unit {
 
+    function addUnit(Unit $unit) {
+        throw new UnitException(get_class($this) . " is a leaf");
+    }
+
+    function removeUnit(Unit $unit) {
+        throw new UnitException(get_class($this) . " is a leaf");
+    }
+
     function bombardStrength() {
         return 44;
     }
 
 }
 
-class Army {
+class Army extends Unit {
 
     private $units = array();
-    private $armies = array();
 
     function addUnit(Unit $unit) {
-        array_push($this->units, $unit);
+        if (in_array($unit, $this->units, true)) {
+            return;
+        }
+        $this->units[] = $unit;
     }
 
-    function addArmy(Army $army) {
-        array_push($this->armies, $army);
+    function removeUnit(Unit $unit) {
+        $this->units = array_udiff($this->units, array($unit), function( $a, $b ) {
+            return ($a === $b) ? 0 : 1;
+        });
     }
 
-    function bombardStrength() {
+    function bombardStrength() { 
         $ret = 0;
         foreach ($this->units as $unit) {
-            $ret+=$unit->bombardStrength();
-        }
-        foreach ($this->armies as $army) {
-            $ret += $army->bombardStrength();
+            $ret += $unit->bombardStrength();
         }
         return $ret;
     }
 
 }
+
+// create an army
+$main_army = new Army();
+// add some units
+$main_army->addUnit( new Archer() );
+$main_army->addUnit( new LaserCannonUnit() );
+
+
+// create a new army
+$sub_army = new Army();
+
+// add some units
+$sub_army->addUnit( new Archer() );
+$sub_army->addUnit( new Archer() );
+$sub_army->addUnit( new Archer() );
+
+// add the second army to the first
+$main_army->addUnit( $sub_army );
+
+// all the calculations handled behind the scenes
+print "attacking with strength: {$main_army->bombardStrength()}\n";
+
